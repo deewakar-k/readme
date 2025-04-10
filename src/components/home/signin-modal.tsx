5bc6feddf5737964e9d28deaf9e8bccec5e08122
deewakar-k <dakshftw01@gmail.com>
2025-04-10 16:10:24 +0000

    projects schema

--- src/components/home/signin-modal.tsx
+++ src/components/home/signin-modal.tsx
@@ -1,40 +1,57 @@
-"use client"
+"use client";
 
-import { toast } from "sonner"
-import { Icons } from "../icons"
-import { Button } from "../ui/button"
-import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog"
-import { signIn } from "@/lib/auth-client"
+import { toast } from "sonner";
+
+import { signIn } from "@/lib/auth-client";
+
+import { Icons } from "../icons";
+import { Button } from "../ui/button";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
 
 export const SignInModal = () => {
+  const handleSignIn = async (provider: "google" | "github") => {
+    toast.promise(
+      signIn.social({
+        provider: provider,
+        callbackURL: "/readme",
+      }),
+      {
+        loading: "redirecting...",
+        success: "redirected successfully",
+        error: "login redirect failed",
+      }
+    );
+  };
 
-	const handleSignIn = async (provider: "google" | "github") => {
-		toast.promise(signIn.social({
-			provider: provider,
-			callbackURL: "/readme"
-		}), {
-			loading: 'redirecting...',
-			success: 'redirected successfully',
-			error: 'login redirect failed'
-		})
-	}
-
-	return (
-		<DialogContent className="sm:max-w-[380px]">
-			<DialogHeader className="font-medium leading-none">
-				<DialogTitle>
-					Welcome to readme 🌱
-				</DialogTitle>
-			</DialogHeader>
-			<DialogDescription>By continuing you agree to our terms of service and privacy policy.</DialogDescription>
-			<div className="flex flex-col gap-2">
-				<Button onClick={() => handleSignIn("google")}>
-					continue with <span> <Icons.google /> </span>
-				</Button>
-				<Button onClick={() => handleSignIn("github")}>
-					continue with <span> <Icons.gitHub /> </span>
-				</Button>
-			</div>
-		</DialogContent>
-	)
-}
+  return (
+    <DialogContent className="sm:max-w-[380px]">
+      <DialogHeader className="leading-none font-medium">
+        <DialogTitle>Welcome to readme 🌱</DialogTitle>
+      </DialogHeader>
+      <DialogDescription>
+        By continuing you agree to our terms of service and privacy policy.
+      </DialogDescription>
+      <div className="flex flex-col gap-2">
+        <Button onClick={() => handleSignIn("google")}>
+          continue with{" "}
+          <span>
+            {" "}
+            <Icons.google />{" "}
+          </span>
+        </Button>
+        <Button onClick={() => handleSignIn("github")}>
+          continue with{" "}
+          <span>
+            {" "}
+            <Icons.gitHub />{" "}
+          </span>
+        </Button>
+      </div>
+    </DialogContent>
+  );
+};
