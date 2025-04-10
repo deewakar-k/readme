96ab7154c383af041aceb32d5be2505641766708
deewakar-k <dakshftw01@gmail.com>
2025-04-10 13:08:13 +0000

    chore

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 import "./src/env/server.ts";
 
 const nextConfig: NextConfig = {
-	images: {
-		domains: ['lh3.googleusercontent.com'],
-	},
-	experimental: {
-		typedRoutes: true,
-	},
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
