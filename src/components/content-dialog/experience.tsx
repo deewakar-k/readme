dbf8eeebba057ae63b4f27a9286a0465813f8f6a
deewakar-k <dakshftw01@gmail.com>
2025-04-13 14:01:13 +0530

    added work more ui

--- src/components/content-dialog/experience.tsx
+++ src/components/content-dialog/experience.tsx
@@ -1,22 +1,54 @@
+import { useState } from "react";
+
 import { works } from "@/utils/mock-data";
 
 import { Add } from "../add-more";
 import Content from "../content";
+import { GoBack } from "../go-back";
+import { InputBox } from "../input-box";
+import { CustomTextArea } from "../text-area";
+import { YearSelector } from "../year-selector";
 
 export const WorkContent = () => {
+  const [add, setAdd] = useState(false);
   return (
     <div className="flex flex-col gap-3">
-      {works.map((work, idx) => (
-        <Content
-          header={work.header}
-          title={work.title}
-          description={work.description}
-          showAction={true}
-          key={idx}
-        />
-      ))}
+      {add ? (
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center gap-2">
+            <InputBox label="oranization" placeholder="Netflix" value="" />
+            <YearSelector />
+          </div>
+          <div className="flex items-center gap-2">
+            <InputBox label="link" placeholder="www.netflix.com" value="" />
+            <InputBox label="role" placeholder="swe" value="" />
+          </div>
+          <CustomTextArea
+            label="description"
+            placeholder="worked on security..."
+            defaultValue=""
+            className="min-h-48"
+          />
+        </div>
+      ) : (
+        <>
+          {works.map((work, idx) => (
+            <Content
+              header={work.header}
+              title={work.title}
+              description={work.description}
+              showAction={true}
+              key={idx}
+            />
+          ))}
+        </>
+      )}
       <div className="fixed right-4 bottom-4">
-        <Add />
+        {add ? (
+          <GoBack handleOnClick={() => setAdd(false)} />
+        ) : (
+          <Add handleOnClick={() => setAdd(true)} />
+        )}
       </div>
     </div>
   );
