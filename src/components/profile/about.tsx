7ea29a8265f4b5f939b6572c476dea3e23801bdf
deewakar-k <dakshftw01@gmail.com>
2025-04-15 21:41:59 +0530

    user about

--- src/components/profile/about.tsx
+++ src/components/profile/about.tsx
@@ -1,11 +1,11 @@
+import { getUser } from "@/actions/user";
+
 export const About = async () => {
+  const user = await getUser();
   return (
     <div className="mt-4 flex flex-col gap-3">
       <h1>About</h1>
-      <p className="text-muted-foreground text-sm">
-        hi!! im deewakar, and i love building stuff on the internet, currently
-        building readme. im really interested in computers and design.
-      </p>
+      <p className="text-muted-foreground text-sm">{user?.about}</p>
     </div>
   );
 };
