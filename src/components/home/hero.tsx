7ad0763b4a1c6877b1059d5c62d2db4440f5c48c
deewakar-k <dakshftw01@gmail.com>
2025-04-21 22:06:08 +0530

    home animations

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,10 +1,37 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export const Hero = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
+
   return (
-    <div className="flex flex-col gap-4 text-center">
-      <h1 className="text-8xl font-semibold">readme.</h1>
-      <p className="text-muted-foreground mx-auto max-w-md font-light">
+    <motion.div
+      initial="hidden"
+      variants={containerVariants}
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="flex flex-col gap-4 text-center"
+    >
+      <motion.h1 variants={itemVariants} className="text-8xl font-semibold">
+        readme.
+      </motion.h1>
+      <motion.p className="text-muted-foreground mx-auto max-w-md font-light">
         elevate your professional presence
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 };
--- src/components/home/waitlist.tsx
+++ src/components/home/waitlist.tsx
@@ -1,17 +1,43 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 import { Dialog, DialogTrigger } from "../ui/dialog";
 import { SignInModal } from "./signin-modal";
 
 export const WaitlistButton = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="relative h-12 w-full max-w-[140px] cursor-pointer rounded-full border border-stone-300 bg-neutral-100 shadow-xl dark:border-stone-700 dark:bg-neutral-800">
+        <motion.div
+          initial="hidden"
+          variants={containerVariants}
+          whileInView="visible"
+          className="relative h-12 w-full max-w-[140px] cursor-pointer rounded-full border border-stone-300 bg-neutral-100 shadow-xl dark:border-stone-700 dark:bg-neutral-800"
+        >
           <div className="relative flex h-full">
-            <div className="z-10 flex flex-1 items-center justify-center text-lg text-black transition-colors duration-300 dark:text-white">
+            <motion.div
+              variants={itemVariants}
+              className="z-10 flex flex-1 items-center justify-center text-lg text-black transition-colors duration-300 dark:text-white"
+            >
               Build Now
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
       </DialogTrigger>
       <SignInModal />
     </Dialog>
