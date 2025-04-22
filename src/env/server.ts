d311e0a460e7d24113a3e61c3d834cbe9118a49e
deewakar-k <dakshftw01@gmail.com>
2025-04-22 20:53:33 +0530

    init pinata

--- src/env/server.ts
+++ src/env/server.ts
@@ -2,23 +2,28 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-	server: {
-		NODE_ENV: z.enum(["development", "production"]),
-		BETTER_AUTH_SECRET: z.string(),
-		DATABASE_URL: z.string(),
-		GOOGLE_CLIENT_ID: z.string(),
-		GOOGLE_CLIENT_SECRET: z.string(),
-		GITHUB_CLIENT_ID: z.string(),
-		GITHUB_CLIENT_SECRET: z.string(),
-	},
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    BETTER_AUTH_SECRET: z.string(),
+    DATABASE_URL: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    PINATA_JWT: z.string(),
+  },
 
-	client: {
-		NEXT_PUBLIC_APP_URL: z.string(),
-	},
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string(),
+    NEXT_PUBLIC_GATEWAY_URL: z.string(),
+  },
 
-	emptyStringAsUndefined: true,
-	//eslint-disable-next-line n/no-process-env
-	experimental__runtimeEnv: {
-		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL
-	},
+  emptyStringAsUndefined: true,
+  experimental__runtimeEnv: {
+    //eslint-disable-next-line n/no-process-env
+    NEXT_PUBLIC_GATEWAY_URL: process.env.NEXT_PUBLIC_GATEWAY_URL,
+
+    //eslint-disable-next-line n/no-process-env
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
 });
